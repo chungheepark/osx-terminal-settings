44ec3844d4956d83e37c55a1b210e1bae7295942
chungheepark <dev.roy.park@gmail.com>
2020-07-04 23:46:38 +0900

    plugin change
    - antibody to oh-my-zsh because of bug
    - change propt pure to dracula
    - change ln -s to ln -sf

--- install.sh
+++ install.sh
@@ -1,37 +1,58 @@
 #!/bin/sh
 
+echo "define var"
 ITERM_THEME='dracula-iterm-theme'
 TERM_THEME='dracula-term-theme'
+ZSH_THEME='dracula-zsh-theme'
 D2_CODING_FONT='d2codingfont'
 
-rm -rf $ITERM_THEME
-git clone https://github.com/dracula/iterm.git $ITERM_THEME
-
-rm -rf $TERM_THEME
-git clone https://github.com/dracula/terminal-app.git $TERM_THEME
-
-rm -rf $D2_CODING_FONT
-git clone https://github.com/naver/d2codingfont.git $D2_CODING_FONT
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh ripgrep
-brew install node
-brew install yarn
-
-npm install --global pure-prompt
-
-sh ./apply_plugin.sh
-
-TERMINAL_PATH=$(pwd)
-
+echo "clear home files"
 rm -rf ~/.alias.sh
 rm -rf ~/.exports.sh
 rm -rf ~/.zshrc
-rm -rf ~/.zsh_plugins.sh
 
-ln -s $TERMINAL_PATH/.alias.sh ~/.alias.sh
-ln -s $TERMINAL_PATH/.exports.sh ~/.exports.sh
-ln -s $TERMINAL_PATH/.zsh_plugins.sh ~/.zsh_plugins.sh
-ln -s $TERMINAL_PATH/.zshrc ~/.zshrc
+echo "install homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh
+brew install ripgrep
+brew install node
+brew install yarn
 
-source ~/.zshrc
+echo "install osx app using cask"
+brew cask install bartender
+brew cask install istat-menus
+brew cask install keka
+brew cask install slack
+brew cask install app-cleaner
+brew cask install karabiner-elements
+brew cask install alfred
+brew cask install gitkraken
+brew cask install zeplin
+brew cask install flipper
+brew cask install postman
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install notion
+brew cask install trello
+
+echo "install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "install dependency"
+
+rm -rf $ITERM_THEME
+rm -rf $TERM_THEME
+rm -rf $D2_CODING_FONT
+rm -rf $ZSH_THEME
+
+git clone https://github.com/dracula/iterm.git $ITERM_THEME
+git clone https://github.com/dracula/terminal-app.git $TERM_THEME
+git clone https://github.com/naver/d2codingfont.git $D2_CODING_FONT
+git clone https://github.com/dracula/zsh.git $ZSH_THEME
+
+ln -sf $(pwd)/.alias.sh ~/.alias.sh
+ln -sf $(pwd)/.exports.sh ~/.exports.sh
+ln -sf $(pwd)/$ZSH_THEME/*.zsh-theme ~/.oh-my-zsh/custom/themes
+ln -sf $(pwd)/.zshrc ~/.zshrc
+
+exec zsh
